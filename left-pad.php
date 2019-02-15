ba5df3a4604bdc1f96803712d5c1e2e8cfeebedf
wilsonbiggs <wilson@wilsonbiggs.com>
2019-02-15 18:10:47 -0500

    Add left pad functionality.

--- left-pad.php
+++ left-pad.php
@@ -8,3 +8,33 @@
  * License: WTFPL
  * License URI: http://www.wtfpl.net/txt/copying/
  */
+
+function left_pad_func( $atts ){
+  // gotta have an input dude
+  if (!array_key_exists(0, $atts)) {
+    return "Error: left-pad requires a string input.";
+  }
+  // defaults
+  // basically do nothing if no input length
+  if (!array_key_exists(1, $atts)) {
+    $atts[1] = 0;
+  }
+  // default to a space.
+  // have to use non-breaking space, WordPress consolidates consecutive spaces
+  if (!array_key_exists(2, $atts)) {
+    $atts[2] = "&nbsp;";
+  }
+  // map attribute array to variables for readability
+  $str = $atts[0];
+  $len = $atts[1];
+  // decode html entities before mapping to variable
+  $ch = html_entity_decode($atts[2]);
+  // limit pad to one character
+  $ch = mb_substr($ch,0,1);
+  // pad!
+  $str = str_pad($str, $len, $ch, STR_PAD_LEFT);
+  // escape html, just to be safe
+  return esc_html($str);
+}
+
+add_shortcode( 'left_pad', 'left_pad_func' );
